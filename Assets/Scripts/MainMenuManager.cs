742e72f90e7c3c18a031a5121438ad6dfbe625d1
YazminSuybate <yaaazscx@gmail.com>
2025-12-02 11:25:34 -0500

    Correccion de la vista principal y transicion

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,13 +1,36 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI; 
 
 public class MainMenuManager : MonoBehaviour
 {
+    public Image panelFade;
+
     public string nombreEscenaJuego = "Mision1";
+
+    public float duracionTransicion = 1.0f;
 
     public void IniciarJuego()
     {
-        SceneManager.LoadScene(nombreEscenaJuego);
+        StartCoroutine(FadeToLevel(nombreEscenaJuego));
     }
 
+    private IEnumerator FadeToLevel(string sceneName)
+    {
+        float tiempo = 0f;
+
+        while (tiempo < duracionTransicion)
+        {
+            tiempo += Time.deltaTime;
+
+            float alpha = Mathf.Clamp01(tiempo / duracionTransicion);
+
+            panelFade.color = new Color(panelFade.color.r, panelFade.color.g, panelFade.color.b, alpha);
+
+            yield return null;
+        }
+
+        SceneManager.LoadScene(sceneName);
+    }
 }
